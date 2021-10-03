fe0873a4ed53ee2371e98daca9fa67acdcd8aaa4
Tomasz Strąk <tomasz.strak@new-gravity.com>
2021-10-03 22:21:43 +0200

    cutom axios call with after

--- src/components/Club.vue
+++ src/components/Club.vue
@@ -7,10 +7,10 @@
 </template>
 
 <script>
-import strava from "strava-v3";
+// import strava from "strava-v3";
 import Members from "@/components/Members";
 import Results from "@/components/Results";
-// import axios from "axios";
+import axios from "axios";
 
 export default {
   name: "Club",
@@ -43,21 +43,19 @@ export default {
       const first =
         new Date(today.getFullYear(), today.getMonth(), 1).getTime() / 1000;
 
-      // this.activities = await axios.get(
-      //   `https://www.strava.com/api/v3/clubs/${this.clubId}/activities?after=${first}`,
-      //   { ...this.config },
-      //   {
-      //     headers: {
-      //       Authorization: `Bearer ${this.config.access_token}`,
-      //     },
-      //   }
-      // );
-      // console.log(this.activities);
-      this.activities = await strava.clubs.listActivities({
-        id: this.clubId,
-        after: first,
-        ...this.config,
-      });
+      try {
+        const { data } = await axios.get(
+          `https://www.strava.com/api/v3/clubs/${this.clubId}/activities?after=${first}`,
+          {
+            headers: {
+              Authorization: `Bearer ${this.config.access_token}`,
+            },
+          }
+        );
+        this.activities = data;
+      } catch (err) {
+        console.log(err);
+      }
     },
   },
   data() {
